2de103e2d6f68f00a7baecea17a6b96621f77aea
unknown <nafizalturabi18cse@gmail.com>
2024-09-09 08:15:24 +0600

    Offer Marquee added

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
+import Offer from '../Shared/Offer';
+import BottomNav from '../Shared/BottomNav';
 
 const Layout = () => {
     return (
         <div>
+            <Offer />
             <Navbar />
             <Outlet />
+            <BottomNav/>
         </div>
     );
 };
---
+++ src/Shared/Offer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+
+const Offer = () => {
+    return (
+        <Marquee 
+            className='bg-[#8c6cf2] text-white py-2' 
+            pauseOnHover={true} 
+            gradient={false} 
+            speed={50} 
+        >
+            <div className="flex space-x-20 lg:space-x-72">
+                <h1 className='text-lg font-bold font-josefin'>
+                    30% OFF - <span className='text-base font-thin text-gray-300'>Offer</span> Ends Tomorrow - Grab Now
+                </h1>
+                <h1 className='text-lg font-bold font-josefin'>
+                    30% OFF - <span className='text-base font-thin text-gray-300'>Offer</span> Ends Tomorrow - Grab Now
+                </h1>
+                <h1 className='text-lg font-bold font-josefin'>
+                    30% OFF - <span className='text-base font-thin text-gray-300'>Offer</span> Ends Tomorrow - Grab Now
+                </h1>
+                
+            </div>
+        </Marquee>
+    );
+};
+
+export default Offer;
