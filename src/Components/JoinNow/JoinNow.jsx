fd46478e720c34a2bb030cc01addd4ced126daad
unknown <nafizalturabi18cse@gmail.com>
2024-09-10 08:52:55 +0600

    Join Now page added

--- src/Components/JoinNow/JoinNow.jsx
+++ src/Components/JoinNow/JoinNow.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 const JoinNow = () => {
     return (
-        <div className='font-josefin container '>
-            <div className='flex flex-col justify-center'>
-                <h1 className='text-[26px] text-center uppercase font-josefin font-bold mx-4 xl:mx-0 font-josefin'>Join The Club</h1>
-                <p className='my-4 text-center'>Join & Get 15% Off On Your First Order, be the first one to know our exciting deals and offers</p>
-                <div>
-                    <form className=''>
-                        <input type="email" className='w-2/5 p-5 border rounded focus:outline-[#9578ed] ' placeholder='Enter you email' />
-                        <button className='p-btn w-2/5 p-5 block'>
+        <div className='font-josefin container mx-auto my-20 px-4'>
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className='text-[22px] sm:text-[26px] text-center uppercase font-bold mx-4 xl:mx-0'>
+                    Join The Club
+                </h1>
+                <p className='my-4 text-center text-sm sm:text-base'>
+                    Join & Get 15% Off On Your First Order, be the first one to know our exciting deals and offers
+                </p>
+                <div className='w-full max-w-xl'>
+                    <div className='flex flex-col items-center'>
+                        <input 
+                            type="email" 
+                            className='w-full p-3 sm:p-4 mb-4 border rounded hover:border-gray-700 focus:outline-[#9578ed]' 
+                            placeholder='Enter your email' 
+                        />
+                        <button 
+                            className="p-btn w-full p-2  text-lg sm:text-2xl rounded text-white hover:shadow-xl hover:-translate-y-[1px] active:shadow-none active:translate-y-[1px] transition-all duration-100">
                             Join now
                         </button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
