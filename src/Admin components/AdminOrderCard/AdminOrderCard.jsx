6e3edfc196cdd327138dc0825f591a18a164482f
unknown <nafizalturabi18cse@gmail.com>
2024-09-20 16:53:50 +0600

    Order added in admin page and status added

--- src/Admin components/AdminOrderCard/AdminOrderCard.jsx
+++ src/Admin components/AdminOrderCard/AdminOrderCard.jsx
@@ -1,51 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../../Axios/axiosInstance';
+import { toast, ToastContainer } from 'react-toastify';
 
-const AdminOrderCard = () => {
+const AdminOrderCard = ({ order }) => {
+    const statusOptions = ['Pending', 'In Process', 'Complete'];
+
+    const [status, setStatus] = useState(order.status);
+
+    const token = localStorage.getItem('authToken')
+
+    const handleStatusChange = async (event) => {
+        const newStatus = event.target.value;
+        setStatus(newStatus);
+
+        try {
+            const response = await axiosInstance.patch(`/adminOrders/${order._id}`, { newStatus }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('status updated succesfully');
+        } catch (error) {
+            toast.error('Failed to update status');
+            console.error('Error updating status:', error);
+        }
+    };
     return (
-        <div className="bg-white shadow-md rounded-lg p-6 mb-4 container mx-auto border-t">
-            {/* Order Header */}
+        <div className="bg-white shadow-lg rounded-lg p-6 mb-4 container mx-auto border-t-4 border-rose-300">
+            <ToastContainer />
             <div className="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-4">
-                <h2 className="text-xl font-bold">Order #12345</h2>
-                <p className="text-gray-500 text-sm">Order Date: 2024-09-18</p>
+                <h2 className="text-base font-bold text-gray-500">Order #{order.transactionId}</h2>
+                <p className="text-gray-500 text-sm">Order Date: {new Date(order.date).toLocaleDateString()}</p>
             </div>
 
-            {/* Order Details */}
-            <div className="flex flex-col md:flex-row md:justify-between items-start md:items-center border-t border-gray-200 pt-4">
+            <div className="border-t w-full border-gray-200 pt-4  grid grid-cols-1 lg:grid-cols-3 xl:flex jxl:ustify-between  gap-6">
                 {/* Customer Info */}
-                <div className="md:w-1/3">
-                    <h3 className="text-md font-semibold text-gray-700">Customer</h3>
-                    <p className="text-gray-600">John Doe</p>
-                    <p className="text-gray-500 text-sm">john.doe@example.com</p>
+                <div className='xl:w-1/4'>
+                    <h3 className="text-md font-semibold text-gray-700 mb-2">Customer Info</h3>
+                    <p className="text-gray-600">Email: {order.email}</p>
+                    <p className="text-gray-500 text-sm">Total Items: {order.totalItems.reduce((acc, item) => acc + item, 0)}</p>
+                    <p className="text-gray-500 text-sm">Total Price: ${order.price}</p>
                 </div>
 
-                {/* Product Info */}
-                <div className="md:w-1/3 mt-4 md:mt-0">
-                    <h3 className="text-md font-semibold text-gray-700">Product</h3>
-                    <p className="text-gray-600">Smartphone X</p>
-                    <p className="text-gray-500 text-sm">Price: $899.99</p>
-                </div>
+                {/* Products Info */}
+                <div className='xl:w-full'>
+                    <h3 className="text-md font-semibold text-gray-700 mb-2 ">Products</h3>
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-2 '>
+                        {order.title.map((product, index) => (
+                            <div key={index} className="mb-4 flex items-start space-x-4 border border-violet-200 p-1 rounded ">
+                                <img
+                                    className="w-16 h-16 object-cover rounded-lg shadow-md"
+                                    src={order.image[index]}
+                                    alt={product}
+                                />
+                                <div className='flex'>
+                                    <div>
+                                        <p className="text-gray-600 font-medium text-xs">{product}</p>
+                                        <p className="text-gray-500 text-sm">Size: {order.size[index]}</p>
+                                        <p className="text-gray-500 text-sm">Total: {order.totalItems[index]}</p>
 
-                {/* Order Status */}
-                <div className="md:w-1/3 mt-4 md:mt-0 flex md:justify-end">
-                    <div>
-                        <h3 className="text-md font-semibold text-gray-700">Status</h3>
-                        <span className="px-4 py-1 rounded-full text-sm font-semibold bg-yellow-100 text-yellow-800">
-                            Pending
-                        </span>
+                                    </div>
+                                    <div className="flex items-center space-x-2">
+                                        <span
+                                            className="w-6 h-6 border"
+                                            style={{ backgroundColor: order.color[index] }}
+                                            title={order.color[index]}
+                                        ></span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </div>
 
-            {/* Actions */}
-            <div className="flex justify-between mt-6">
-                {/* Approve Button */}
-                <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md">
-                    Approve
-                </button>
-                {/* Cancel Button */}
-                <button className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md">
-                    Cancel Order
-                </button>
+                {/* Status Dropdown */}
+                <div className='xl:w-1/4'>
+                    <h3 className="text-md font-semibold text-gray-700 mb-2">Order Status</h3>
+                    <select
+                        value={status}
+                        onChange={handleStatusChange}
+                        className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                        {statusOptions.map(option => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+
+                </div>
             </div>
         </div>
     );
--- src/Admin components/Orders/Orders.jsx
+++ src/Admin components/Orders/Orders.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import AdminOrderCard from '../AdminOrderCard/AdminOrderCard';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from '../../Axios/axiosInstance';
+import Loading from '../../Components/Loading/Loading';
 
 const Orders = () => {
+    const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('authToken');
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const { isLoading, isError, data = [], error, refetch } = useQuery({
+        queryKey: ['userOrders'],
+        queryFn: async () => {
+            const response = await axiosInstance.get('/adminOrders', {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            return response.data;
+        },
+    });
+    
+    if (isLoading) return <Loading />;
+
+    if (isError) {
+        console.error('Error:', error);
+        return <p>Error loading orders: {error.message}</p>;
+    }
+
+    const sortedData = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    // Calculate total pages
+    const totalPages = Math.ceil(sortedData.length / itemsPerPage);
+
+    // Get the current items
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const currentItems = data.slice().reverse().slice(startIndex, startIndex + itemsPerPage);
     return (
         <div className='mt-10'>
-            <AdminOrderCard/>
+            {
+                currentItems.map(order =>
+                    <AdminOrderCard key={order._id} order={order}/>
+                )
+            }
+
+            {/* Pagination Controls */}
+            {totalPages > 1 && (
+                <div className="flex justify-center space-x-2 mt-4">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                            key={index + 1}
+                            onClick={() => setCurrentPage(index + 1)}
+                            className={`px-3 py-1 rounded ${currentPage === index + 1 ? 'bg-violet-500 text-white' : 'bg-gray-300 text-black'}`}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
